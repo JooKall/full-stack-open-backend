bbf1e1866ab16f5e2212e99f7476c28789fefc0c
Joonas Kallio <kallio.joonas@proton.me>
2024-09-24 16:29:23 +0300

    3.12

---
+++ puhelinluettelo_backend/mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give at least password as a argument.')
+    console.log('if you would like to add a person give also the name and number of the person to be added.')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://jookal:${password}@cluster0.skwvz.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+if (process.argv.length === 3) {
+
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+
+} else if (process.argv.length === 5) {
+
+    const newName = process.argv[3]
+    const newNumber = process.argv[4]
+
+    const person = new Person({
+        name: newName,
+        number: newNumber
+    })
+
+    person.save().then(result => {
+        console.log(`added ${newName} number ${newNumber} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log('give at least the password, and optionally the name AND number of the person to be added.');
+    mongoose.connection.close().then(() => {
+        process.exit(1); // Exit after the connection is closed
+    });
+}
