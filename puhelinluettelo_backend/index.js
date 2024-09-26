c4c21d61bda0f28c049d993af8efa89227c75905
Joonas Kallio <kallio.joonas@proton.me>
2024-09-26 13:14:37 +0300

    uibuild

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -20,7 +20,7 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ message: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ name: error.name, message: error.message })
   }
